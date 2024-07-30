b9994989ca235ce1c823b127ec135d660b6d2d57
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-07-29 21:57:48 -0300

    directories reorganization

---
+++ rust-projects/guessing_game/target/debug/deps/guessing_game-2aff2e55542e0262.d
@@ -0,0 +1,5 @@
+/home/guilherme/Coding/learning-rust/rust-projects/guessing_game/target/debug/deps/guessing_game-2aff2e55542e0262: src/main.rs
+
+/home/guilherme/Coding/learning-rust/rust-projects/guessing_game/target/debug/deps/guessing_game-2aff2e55542e0262.d: src/main.rs
+
+src/main.rs:
