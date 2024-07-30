6f8fb5964e36f3c1400765e9004830dfa1ed69cf
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-07-29 21:58:23 -0300

    directories reorganization

---
+++ rust-projects/guessing_game/target/debug/deps/rand_chacha-81617e6b2a0e386e.d
@@ -0,0 +1,9 @@
+/home/guilherme/Coding/learning-rust/rust-projects/guessing_game/target/debug/deps/librand_chacha-81617e6b2a0e386e.rmeta: /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/home/guilherme/Coding/learning-rust/rust-projects/guessing_game/target/debug/deps/librand_chacha-81617e6b2a0e386e.rlib: /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/home/guilherme/Coding/learning-rust/rust-projects/guessing_game/target/debug/deps/rand_chacha-81617e6b2a0e386e.d: /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
+/home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
+/home/guilherme/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
---
+++ rust-projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/guilherme/Coding/learning-rust/rust-projects/guessing_game/target/debug/guessing_game: /home/guilherme/Coding/learning-rust/rust-projects/guessing_game/src/main.rs
